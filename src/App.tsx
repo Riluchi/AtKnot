8d21714889c0a887f1dc1ad154b552f7871a1eaa
Riluchi <enlil.misskey.io@gmail.com>
2026-03-15 10:21:21 +0900

    add atknot app files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Atknot Web</h1>
+      <p>Web版の初期構築が完了しました。</p>
+    </main>
+  );
+}
